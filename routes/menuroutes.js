f524e9216cf1a1a23fde68b6b4e3e439a86cb5a3
Tushar sharma <pandit2007tushar@gmail.com>
2026-02-02 21:00:40 +0530

    delete and put method added in menuroutes

--- routes/menuroutes.js
+++ routes/menuroutes.js
@@ -41,4 +41,38 @@ router.get('/:rate',async (req,res)=>{
         res.status(500).json({error:'internal server error'});
     }
 })
+router.put('/:id',async(req,res)=>{
+    try{
+        const id=req.params.id;
+        const data=req.body;
+        const updated_data= await menu.findByIdAndUpdate(id,data,{
+            new:true,
+            runvalidators:true
+        })
+        if(!updated_data){
+            console.log('menu item not found');
+            res.status(404).json({error:'menu item not found'});
+        }
+        console.log('updated successfully');
+        res.status(200).json(updated_data);
+    }catch(err){
+        console.log('internal server error');
+        res.status(500).json({error:'internal server error'});
+    }
+})
+router.delete('/:id',async(req,res)=>{
+    try{
+        const id= req.params.id;
+        const del_data= await menu.findByIdAndDelete(id);
+        if(!del_data){
+            console.log('no item found of this id');
+            res.status(404).json({error:'no item found of this id'});
+        }
+        res.status(200).send({done:'menu item deleted successfully'});
+        console.log('menu item deleted successfully');
+    }catch(err){
+        console.log('internal server error');
+        res.status(500).json({error:'internal server error'});
+    }
+})
 module.exports=router;
